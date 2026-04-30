70335f48cae1fb14f993e98a82106c64afb6d557
Mahathi Kethineni <mahathikethineni@gmail.com>
2026-04-30 22:26:15 +0000

    project name mismatch in dbt_project.yml file

--- macros/gennerate_schema_name.sql
+++ macros/gennerate_schema_name.sql
@@ -1,20 +1,20 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {{ "FORCED_SCHEMA" }}
+    -- {{ "FORCED_SCHEMA" }}
 
-    -- {%- set default_schema = target.schema -%}
-    -- {%- set env = env_var('DBT_ENV_NAME') -%}
+    {%- set default_schema = target.schema -%}
+    {%- set env = env_var('DBT_ENV_NAME') -%}
 
     -- {{ log("DEBUG: target.name is [" ~ target.name ~ "] and custom_schema_name is [" ~ custom_schema_name ~ "]", info=True) }}
 
-    -- {%- if custom_schema_name is not none or target.name != 'prod' -%}
+    {%- if custom_schema_name is not none or target.name != 'prod' -%}
 
-    --     {{ default_schema }}
+        {{ default_schema }}
 
-    -- {%- else -%}
+    {%- else -%}
     
-    --     {{ custom_schema_name | trim }}
+        {{ custom_schema_name | trim }}
 
-    -- {%- endif -%}
+    {%- endif -%}
 
 {%- endmacro %}
